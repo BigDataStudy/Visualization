18c02e9fcda041cbada8c185bf7041aa01da35b2
xiulankaixin <602813011@qq.com>
2016-07-26 19:05:35 +0800

    update echart to show last week and next week's chart

--- didi/src/main/java/com/ibm/bigdata/didi/RequestServlet.java
+++ didi/src/main/java/com/ibm/bigdata/didi/RequestServlet.java
@@ -1,10 +1,10 @@
 package com.ibm.bigdata.didi;
 
 import java.io.IOException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Iterator;
 import java.util.List;
 
 import javax.servlet.ServletConfig;
@@ -21,8 +21,6 @@ import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
 import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.Result;
-import org.apache.hadoop.hbase.client.ResultScanner;
-import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.util.Bytes;
 
@@ -71,28 +69,24 @@ public class RequestServlet extends HttpServlet {
 			HttpServletResponse response) throws ServletException, IOException {
 		String district = request.getParameter("district");
 		String date = request.getParameter("datett");
+		System.out.println(date);
 		String output;
 		if (district == null || district == "") {
 			district = "f9280c5dab6910ed44e518248048b9fe";
 		}
-		if (date == null || date == "") {
-			List<String> dateList = getAWeekDates();
-			List<Series> srList = new ArrayList<Series>();
-			for(String dateStr : dateList){
-				Result rs = getRsByDate(district, dateStr);
-				Series sr = getSeries(rs, district, dateStr);
-				if (sr != null){
-					srList.add(sr);
-				}
+		List<String> dateList = getAWeekDates(date);
+		List<Series> srList = new ArrayList<Series>();
+		for(String dateStr : dateList){
+			Result rs = getRsByDate(district, dateStr);
+			Series sr = getSeries(rs, district, dateStr);
+			if (sr != null){
+				srList.add(sr);
+			}else{
+				srList.add(null);
 			}
-			output = JSON.toJSONString(srList);
-		} else {
-			Result rs = getRsByDate(district, date);
-			Series sr = getSeries(rs, district, date);
-			output = JSON.toJSONString(sr);
 		}
+		output = JSON.toJSONString(srList);
 		response.getWriter().append(output);
-		// response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
@@ -131,46 +125,7 @@ public class RequestServlet extends HttpServlet {
 	}
 	
 	
-	private ResultScanner getRsByWeek(String district, List<String> dateList)
-			throws IOException {
-		/**
-		 * A lightweight handle to a specific table. Used from a single thread.
-		 */
-		Table table = null;
-		try {
-			if ( null == connection ||  connection.isClosed() ){
-				this.init(null);
-			}
-			table = connection.getTable(TABLE_NAME);
-			byte[] startRow = Bytes.toBytes(district.concat("-").concat(
-					dateList.get(0)));
-			byte[] stopRow = Bytes.toBytes(district.concat("-").concat(
-					dateList.get(dateList.size() - 1)));
-
-			Scan scan = new Scan(startRow, stopRow);
-
-			return table.getScanner(scan);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			// close everything down
-			if (table != null)
-				table.close();
-		}
-		return null;
-	}
-
-	private List<Series> getSeriesList(ResultScanner rsc, String district) {
-		List<Series> all = new ArrayList<Series>();
-		if ( rsc.iterator() != null ){
-			Iterator<Result> it = rsc.iterator(); 
-			if (it.hasNext()) {
-				getSeries(it.next(), district, "");
-			}
-		}
-			
-		return all;
-	}
+	
 
 	private Series getSeries(Result rs, String district, String date) {
 		if (rs != null && !rs.isEmpty()) {
@@ -204,18 +159,64 @@ public class RequestServlet extends HttpServlet {
 		return null;
 	}
 
-	private List<String> getAWeekDates() {
+	private List<String> getAWeekDates(String date){
 		List<String> curWeekDates = new ArrayList<String>();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		Calendar calendar = Calendar.getInstance();
+		if(date != null && !"".equals(date)){
+			try {
+				calendar.setTime(sdf.parse(date));
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+		}
 		calendar.add(Calendar.DATE, -6);
 		for (int i = 0; i < 7; i++) {
 			String dateStr = sdf.format(calendar.getTime());
 			curWeekDates.add(dateStr);
 			calendar.add(Calendar.DATE, 1);
 		}
-
 		return curWeekDates;
 	}
 
+	/*private ResultScanner getRsByWeek(String district, List<String> dateList)
+	throws IOException {
+		*//**
+		 * A lightweight handle to a specific table. Used from a single thread.
+		 *//*
+		Table table = null;
+		try {
+			if ( null == connection ||  connection.isClosed() ){
+				this.init(null);
+			}
+			table = connection.getTable(TABLE_NAME);
+			byte[] startRow = Bytes.toBytes(district.concat("-").concat(
+					dateList.get(0)));
+			byte[] stopRow = Bytes.toBytes(district.concat("-").concat(
+					dateList.get(dateList.size() - 1)));
+		
+			Scan scan = new Scan(startRow, stopRow);
+		
+			return table.getScanner(scan);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// close everything down
+			if (table != null)
+				table.close();
+		}
+		return null;
+		}*/
+		
+		/*private List<Series> getSeriesList(ResultScanner rsc, String district) {
+		List<Series> all = new ArrayList<Series>();
+		if ( rsc.iterator() != null ){
+			Iterator<Result> it = rsc.iterator(); 
+			if (it.hasNext()) {
+				getSeries(it.next(), district, "");
+			}
+		}
+			
+		return all;
+		}*/
 }
